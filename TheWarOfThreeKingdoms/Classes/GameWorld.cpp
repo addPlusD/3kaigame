32d363de198c317af0c646bede68dc57903b4f98
bonlai <bonlai888@hotmail.com>
2017-04-11 22:48:09 +0800

    Update GameWorld.cpp

--- TheWarOfThreeKingdoms/Classes/GameWorld.cpp
+++ TheWarOfThreeKingdoms/Classes/GameWorld.cpp
@@ -32,6 +32,7 @@ bool GameWorld::init(){
         return false;
     }
     
+    setCards();
     
     //tell program to do update
     scheduleUpdate();
@@ -44,4 +45,23 @@ void GameWorld::update(float delta){
     //do the update job here
     
     CCLOG("Updating...%f", delta);
+}
+
+void GameWorld::setCards() {
+    
+	auto card1 = Card::create("character1.png",9999,0);
+	this->addChild(card1, 1);
+
+	auto card2 = Card::create("character2.png", 998, 1);
+	this->addChild(card2, 1);
+
+	auto card3 = Card::create("arrow.png", 2, 2);
+	this->addChild(card3, 1);
+
+	auto card4 = Card::create("horse.png", 23, 3);
+	this->addChild(card4, 1);
+
+	auto card5 = Card::create("soldier.png", 242, 4);
+	this->addChild(card5, 1);
+    
 }
