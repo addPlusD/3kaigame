5a6b146e4ddee3bc55a232d468f8636f6c962ce2
bonlai <bonlai888@hotmail.com>
2017-04-11 22:41:14 +0800

    Add files via upload

---
+++ TheWarOfThreeKingdoms/Classes/Card.h
@@ -0,0 +1,28 @@
+#ifndef _CARD_HPP_
+#define _CARD_HPP_
+
+#include <stdio.h>
+#include "cocos2d.h"
+#include <string>
+
+
+class Card:public cocos2d::Sprite
+{
+public:
+	Card();
+	~Card();
+
+	static Card* create(const std::string& _file, int characterId, int position);
+
+	void initOptions(int position);
+
+	void addEvents();
+	void setCharacterId(int id);
+	int getCharacterId();
+//	void touchEvent(cocos2d::Touch* touch, cocos2d::Vector2 _p);
+
+private:
+	int characterId;
+};
+#endif // _CARD_HPP_
+
